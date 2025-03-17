bab6554aea22db7f257895af6734097eeecc8e0c
hahahaalol <ggjam1234@gmail.com>
2025-03-17 18:40:31 -0400

    Update bookwork.lua

--- bookwork.lua
+++ bookwork.lua
@@ -1,6 +1,6 @@
 shared["F​F​l​a​g​A​X​C​o​m​b​i​n​e​G​e​t​O​u​t​f​i​t​D​i​s​p​a​t​c​h​e​s​I​X​P​2"] = {
     ["Main"] = {
-        ['Loader'] = { ['Key'] = ('DUr0z1kYU674f0DSsRO4OpFIGpjVJyat') },
+        ['Loader'] = { ['Key'] = ('znwLvaYrnoodmlObOwg3uz3EMDiXr9a7') },
     },
     
     ["Universal"] = {
